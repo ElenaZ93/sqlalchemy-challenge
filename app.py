dd32ad4249947538b3209ecb9753082cf919661f
Elena <elena.zamudio21@gmail.com>
2024-08-08 13:03:59 -0600

    Resources
    raw data / resources

---
+++ app.py
@@ -0,0 +1,31 @@
+# Import the dependencies.
+import numpy as pd
+import pandas as pd
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
